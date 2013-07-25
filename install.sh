ce4162e5ae61f78504f64b735e5b9393888f0083
Ah-Lun Tang <tang@ahlun.be>
2013-07-25 17:51:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,6 +31,8 @@ fi
 echo "Creating .vimrc and .vim in homefolder..."
 ln -s ${DIR}/vimconfig/vim ~/.vim
 ln -s ${DIR}/vimconfig/vimrc ~/.vimrc
+
+echo "Creating backup and undo dir..."
 mkdir ~/.vimscratch
 mkdir ~/.vimscratch/undo
 mkdir ~/.vimscratch/backup
