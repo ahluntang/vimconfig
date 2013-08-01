69ece16e6056b8c65dab316d120d08ac74a205a5
Ah-Lun Tang <tang@ahlun.be>
2013-08-01 10:26:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ then
 fi
 
 echo "Cloning vimconfig..."
-hash git >/dev/null && /usr/bin/env git clone git@github.com:ahluntang/vimconfig.git ${DIR}/vimconfig || {
+hash git >/dev/null && /usr/bin/env git clone https://github.com/ahluntang/vimconfig.git ${DIR}/vimconfig || {
   echo "${red}Could not retrieve the vimconfigs! Is git working?{textreset}"
   exit
 }
